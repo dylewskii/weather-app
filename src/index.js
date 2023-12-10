6bbcde28c7d32bdab6da648437dc0dd9e1fd549e
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-10 22:35:27 +0100

    replace imgs with svgs & edit webpack config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./styles.css";
+import search from "./assets/icons/search.png";
--- webpack.common.js
+++ webpack.common.js
@@ -17,10 +17,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/icons/[name][ext]"
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name][ext]"
+        }
       },
     ],
   },
