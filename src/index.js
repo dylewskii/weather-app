a4eaab495715efdd61e9952787e0a5e72d25e3c7
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-11 14:39:10 +0100

    retrieve & store weather data in object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import "./styles.css";
 // eslint-disable-next-line no-unused-vars
 import search from "./assets/icons/search.png";
+
+const getWeatherData = async (location) => {
+    const api = `https://api.weatherapi.com/v1/current.json?key=1111111111&q=${location}`;
+    const response = await fetch(api, {mode: "cors"});
+    const data = await response.json();
+
+    return data;
+}
+
+const weatherInfo = {
+    location: [],
+    description: null,
+    temperature : null,
+    airQuality : null,
+    pressure: null,
+    uv : null,
+    precipitation : null,
+    wind : null,
+    visibility : null,
+    sunrise : null,
+    sunset : null
+};
+
+getWeatherData("london")
+    .then((data) => {
+        console.log(data)
+        weatherInfo.location = [data.location.name, data.location.region];
+        // weatherInfo.location.push(data.location.name, data.location.region);
+        weatherInfo.description = data.current.condition.text;
+        weatherInfo.temperature = data.current.temp_c;
+        weatherInfo.airQuality = null;
+        weatherInfo.pressure = data.current.pressure_mb;
+        weatherInfo.uv = data.current.uv;
+        weatherInfo.precipitation = null;
+        weatherInfo.wind = data.current.wind_kph;
+        weatherInfo.visibility = data.current.wind_kph;
+        weatherInfo.sunrise = null;
+        weatherInfo.sunset = null;
+    })
