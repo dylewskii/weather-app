576326ceaa17a5269376a57be3608cc0ec2e8b81
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-12 13:41:50 +0100

    add toggle to header

--- src/index.js
+++ src/index.js
@@ -7,20 +7,23 @@ import search from "./assets/icons/search.png";
 const searchBox = document.getElementById("search");
 const searchBtn = document.querySelector(".search-btn");
 // Weather Fields
-const description = document.getElementById("description");
-const temperature = document.getElementById("temperature");
-const airQuality = document.getElementById("air-quality");
-const pressure = document.getElementById("pressure");
-const uv = document.getElementById("uv");
-const precipitation = document.getElementById("precipitation");
-const wind = document.getElementById("wind");
-const visibility = document.getElementById("visibility");
-const sunrise = document.getElementById("sunrise");
-const sunset = document.getElementById("sunset");
+const cityField = document.getElementById("city");
+// const region = document.getElementById("region");
+// const description = document.getElementById("description");
+const temperatureField = document.getElementById("temperature");
+const airQualityField = document.getElementById("air-quality");
+const pressureField = document.getElementById("pressure");
+const uvField = document.getElementById("uv");
+const precipitationField = document.getElementById("precipitation");
+const windField = document.getElementById("wind");
+const visibilityField = document.getElementById("visibility");
+const sunriseField = document.getElementById("sunrise");
+const sunsetField = document.getElementById("sunset");
 
 const weatherInfo = {
-    location: [],
-    description: null,
+    city: null,
+    // region: null,
+    // description: null,
     temperature : null,
     airQuality : null,
     pressure: null,
@@ -33,7 +36,7 @@ const weatherInfo = {
 };
 
 const getWeatherData = async (city) => {
-    const forecastWeatherApi = `https://api.weatherapi.com/v1/forecast.json?key=11111111&q=${city}&aqi=yes`;
+    const forecastWeatherApi = `https://api.weatherapi.com/v1/forecast.json?key=d04037fa261e40e39e0142607230612&q=${city}&aqi=yes`;
     const forecastWeatherResponse = await fetch(forecastWeatherApi, {mode: "cors"});
     const forecastWeatherData = await forecastWeatherResponse.json();
     console.log(forecastWeatherData)
@@ -45,8 +48,9 @@ const saveWeatherData = async (city) => {
     await getWeatherData(city)
         .then((data) => {
             console.log(data)
-            weatherInfo.location = [data.location.name, data.location.region];
-            weatherInfo.description = data.current.condition.text;
+            weatherInfo.city = data.location.name;
+            // weatherInfo.region = data.location.region;
+            // weatherInfo.description = data.current.condition.text;
             weatherInfo.temperature = data.current.temp_c;
             weatherInfo.airQuality = data.current.air_quality["us-epa-index"];
             weatherInfo.pressure = data.current.pressure_mb;
@@ -64,7 +68,7 @@ const saveWeatherData = async (city) => {
 }
 
 const renderData = () => {
-    const fields = [description, temperature, uv, airQuality, pressure, visibility, precipitation, wind, sunrise, sunset];
+    const fields = [cityField, temperatureField, uvField, airQualityField, pressureField, visibilityField, precipitationField, windField, sunriseField, sunsetField];
     fields.forEach((field) => {
         const fieldName = field.id;
         const weatherInfoValue = weatherInfo[fieldName];
